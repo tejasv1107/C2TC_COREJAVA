d2971f66bafa9182bba5c6f9dd5dad34734d56f9
Dell <Dell@DESKTOP-JFCF2E6>
2022-09-07 15:39:24 +0530

    First commit

---
+++ java project/src/com/tns/Myfirstjavaprogram.java
@@ -0,0 +1,20 @@
+package com.tns;
+
+public class Myfirstjavaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 5;
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+}
+}

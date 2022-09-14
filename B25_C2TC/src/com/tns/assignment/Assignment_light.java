95fdbb3c3ef483c1cc51bade9a5dcea30d7a435f
Dell <Dell@DESKTOP-JFCF2E6>
2022-09-14 19:28:56 +0530

    FIRST COMMIT

---
+++ B25_C2TC/src/com/tns/assignment/Assignment_light.java
@@ -0,0 +1,24 @@
+package com.tns.assignment;
+
+import java.util.Scanner;
+
+public class Assignment_light {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner sc=new Scanner(System.in);
+		        int hrs=sc.nextInt();
+		        if(hrs<=18)
+		        {
+		            System.out.println("lights on");
+		        }
+		        else{
+		        System.out.println("lights off");    
+		        }
+		        
+		    }
+		
+	}
+
+
